289d3a79a8e9118e830764166dfc1d3f811091dd
oppen123 <161986254+oppen123@users.noreply.github.com>
2024-07-21 15:05:53 +0800

    update

---
+++ scripts/07_fw3.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# 回滚 iptables 为 1.8.7（1.8.8有一个致命bug，当上游升级至1.8.9时可以去除下面3行；具体参见https://www.netfilter.org/projects/iptables/files/changes-iptables-1.8.9.txt）
+rm -rf ./package/network/utils/iptables
+cp -rf ../openwrt_22/package/network/utils/iptables ./package/network/utils/iptables
+cp -rf ../lede/package/network/utils/iptables/patches/900-bcm-fullconenat.patch ./package/network/utils/iptables/patches/900-bcm-fullconenat.patch
+# 滚回fw3
+pushd feeds/luci
+patch -Rp1 <../../../PATCH/firewall/luci-app-firewall_add_fullcone_fw4.patch
+patch -p1 <../../../PATCH/firewall/luci-app-firewall_add_fullcone_fw3.patch
+popd
+sed -i 's,iptables-nft,iptables-legacy,g' ./package/new/luci-app-passwall2/Makefile
+sed -i 's,iptables-nft,iptables-legacy,g' ./package/new/luci-app-passwall/Makefile
+sed -i 's,iptables-nft +kmod-nft-fullcone,iptables-mod-fullconenat,g' ./package/new/addition-trans-zh/Makefile
+rm -rf ./feeds/packages/net/miniupnpd
+cp -rf ../immortalwrt_pkg_21/net/miniupnpd ./feeds/packages/net/miniupnpd
+wget https://github.com/miniupnp/miniupnp/commit/0e8c68d.patch -O feeds/packages/net/miniupnpd/patches/0e8c68d.patch
+sed -i 's,/miniupnpd/,/,g' ./feeds/packages/net/miniupnpd/patches/0e8c68d.patch
+wget https://github.com/miniupnp/miniupnp/commit/21541fc.patch -O feeds/packages/net/miniupnpd/patches/21541fc.patch
+sed -i 's,/miniupnpd/,/,g' ./feeds/packages/net/miniupnpd/patches/21541fc.patch
+wget https://github.com/miniupnp/miniupnp/commit/b78a363.patch -O feeds/packages/net/miniupnpd/patches/b78a363.patch
+sed -i 's,/miniupnpd/,/,g' ./feeds/packages/net/miniupnpd/patches/b78a363.patch
+wget https://github.com/miniupnp/miniupnp/commit/8f2f392.patch -O feeds/packages/net/miniupnpd/patches/8f2f392.patch
+sed -i 's,/miniupnpd/,/,g' ./feeds/packages/net/miniupnpd/patches/8f2f392.patch
+wget https://github.com/miniupnp/miniupnp/commit/60f5705.patch -O feeds/packages/net/miniupnpd/patches/60f5705.patch
+sed -i 's,/miniupnpd/,/,g' ./feeds/packages/net/miniupnpd/patches/60f5705.patch
+rm -rf ./feeds/luci/applications/luci-app-upnp
+cp -rf ../immortalwrt_luci_21/applications/luci-app-upnp ./feeds/luci/applications/luci-app-upnp
+sed -i '/firewall/d' ./.config
+sed -i '/offload/d' ./.config
+sed -i '/tables/d' ./.config
+sed -i '/nft/d' ./.config
+sed -i '/Nft/d' ./.config
+sed -i '/homeproxy/d' ./.config
+echo '
+CONFIG_PACKAGE_firewall=y
+# CONFIG_PACKAGE_firewall4 is not set
+# CONFIG_PACKAGE_iptables-nft is not set
+CONFIG_PACKAGE_iptables-zz-legacy=y
+# CONFIG_PACKAGE_ip6tables-nft is not set
+CONFIG_PACKAGE_ip6tables-zz-legacy=y
+CONFIG_PACKAGE_xtables-legacy=y
+# CONFIG_PACKAGE_xtables-nft is not set
+CONFIG_PACKAGE_kmod-nft-offload=n
+CONFIG_PACKAGE_kmod-ipt-offload=y
+CONFIG_PACKAGE_dnsmasq_full_ipset=y
+CONFIG_PACKAGE_dnsmasq_full_nftset=n
+CONFIG_PACKAGE_iptables-mod-fullconenat=y
+' >>./.config
+rm -rf ./feeds/luci/applications/luci-app-zerotier
+cp -rf ../lede_luci/applications/luci-app-zerotier ./feeds/luci/applications/luci-app-zerotier
+wget -P feeds/luci/applications/luci-app-zerotier/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/move_2_services.sh
+chmod -R 755 ./feeds/luci/applications/luci-app-zerotier/move_2_services.sh
+pushd feeds/luci/applications/luci-app-zerotier
+bash move_2_services.sh
+popd
+ln -sf ../../../feeds/luci/applications/luci-app-zerotier ./package/feeds/luci/luci-app-zerotier
+
+exit 0
---
+++ scripts/X86/02_target_only.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sed -i 's/O2/O2 -march=x86-64-v2/g' include/target.mk
+
+# libsodium
+sed -i 's,no-mips16 no-lto,no-mips16,g' feeds/packages/libs/libsodium/Makefile
+
+echo '# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
+grep "Default string" /tmp/sysinfo/model >> /dev/null
+if [ $? -ne 0 ];then
+    echo should be fine
+else
+    echo "Generic PC" > /tmp/sysinfo/model
+fi
+
+exit 0
+'> ./package/base-files/files/etc/rc.local
+
+# enable smp
+echo '
+CONFIG_X86_INTEL_PSTATE=y
+CONFIG_SMP=y
+' >>./target/linux/x86/config-5.15
+
+#Vermagic
+latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][3-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/x86/64/packages/Packages.gz
+zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+
+# 预配置一些插件
+cp -rf ../PATCH/files ./files
+
+find ./ -name *.orig | xargs rm -f
+find ./ -name *.rej | xargs rm -f
+
+exit 0
